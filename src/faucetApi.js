e5796115d0baedfaed23fde6863a246c5a081b05
Xiaoyao Qian <jivepaulrock@gmail.com>
2017-01-31 11:13:52 -0600

    promises

--- src/faucetApi.js
+++ src/faucetApi.js
@@ -1,5 +1,6 @@
 var request = require('superagent');
 var Web3 = require('web3');
+var Promise = require('promise');
 
 function RopstenFaucet(httpProvider, verbose) {
   this.ENDPOINT_PREFIX = "http://faucet.ropsten.be:3001/donate/";
@@ -8,19 +9,24 @@ function RopstenFaucet(httpProvider, verbose) {
   this.verbose = verbose
 }
 
-RopstenFaucet.prototype.sendOneEther = function(address, callback) {
-  request
-    .get(this.ENDPOINT_PREFIX + address)
-    .end(function(err, resp) {
-      if (err) {
-        console.error("Error sending ether: ", err);
-      } else if (resp.body.amount > 0) {
-        console.log("Sending ether to %s...", address);
-        callback(address);
-      } else {
-        console.log("What happened?! ", resp.body);
-      }
-    });
+RopstenFaucet.prototype.sendOneEther = function(address) {
+  var _this = this;
+  return new Promise(function(fulfill, reject){
+    request
+      .get(_this.ENDPOINT_PREFIX + address)
+      .end(function(err, resp) {
+        if (err) {
+          console.error("Error sending ether: ", err);
+          reject(err);
+        } else if (resp.body.amount > 0) {
+          console.log("Sending ether to %s...", address);
+          fulfill();
+        } else {
+          console.log("What happened?! ", resp.body);
+          reject(resp.body);
+        }
+      });
+  });
 }
 
 RopstenFaucet.prototype.sendEthers = function(address, numEthers) {
@@ -29,7 +35,13 @@ RopstenFaucet.prototype.sendEthers = function(address, numEthers) {
     var newAccount = this.web3.personal.newAccount("");
     var donateFn = function(account) {
       return function() {
-        _this.sendOneEther(account, _this.waitForDonationToSettle(_this.funnel(account, address)));
+        _this.sendOneEther(account)
+        .then(function(){
+          return _this.waitForDonationToSettle(account);
+        })
+        .then(function(){
+          return _this.funnel(account, address);
+        });
       }
     };
     console.log("Scheduled to ask for 1 ether to %s in %s", newAccount, i*this.DRIP_RATE);
@@ -38,35 +50,35 @@ RopstenFaucet.prototype.sendEthers = function(address, numEthers) {
 }
 
 RopstenFaucet.prototype.funnel = function(from, to) {
-  var _this = this;
-  return function() {
-    console.log("Funneling 1 ether from %s to %s", from, to);
-    _this.web3.personal.unlockAccount(from, "");
-    return _this.web3.eth.sendTransaction({
-      from: from,
-      to: to,
-      value: _this.web3.toWei(0.9, "ether")
-    });
-  }
+  console.log("Funneling 1 ether from %s to %s", from, to);
+  this.web3.personal.unlockAccount(from, "");
+  return this.web3.eth.sendTransaction({
+    from: from,
+    to: to,
+    value: this.web3.toWei(0.9, "ether")
+  });
 }
 
-RopstenFaucet.prototype.waitForDonationToSettle = function(callback) {
+RopstenFaucet.prototype.waitForDonationToSettle = function(address) {
   var _this = this;
-  return function(address) {
+  var fn = function(fulfill) {
     if (_this.verbose) {
       console.log("Waiting for the 1 ether donation to %s to settle...", address);
     }
-    
+
     if (_this.web3.eth.getBalance(address).toNumber() < _this.web3.toWei(1, "ether")) {
-      setTimeout(function() {
-        _this.waitForDonationToSettle(callback)(address);
-      }, 5000);
+      setTimeout(function() {fn(fulfill);}, 5000);
     } else {
-      if (callback) {
-        callback();
+      if (_this.verbose) {
+        console.log("1 ether donation has arrived at %s", address);
       }
+      fulfill();
     }
   }
+
+  return new Promise(function(fulfill, reject){
+    fn(fulfill);
+  });
 }
 
 module.exports.RopstenFaucet = RopstenFaucet;
